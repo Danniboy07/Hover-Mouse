9851a394b2720a43c66ef6fbcd28894fd308f8c5
Dhanvant <92245578+Danniboy07@users.noreply.github.com>
2022-08-11 22:26:27 +0530

    revert to optimised older pointer model

--- tracker.py
+++ tracker.py
@@ -6,6 +6,7 @@ screen_w, screen_h = pyautogui.size()
 cam = cv2.VideoCapture(0)
 pyautogui.FAILSAFE = False
 hand = mp.solutions.hands.Hands()
+old_x, old_y = 0, 0
 
 def displacement(x, x1, y, y1):
     dis = round(((x1-x)**2 + (y1-y)**2)**0.5)
@@ -15,9 +16,6 @@ def displacement(x, x1, y, y1):
 
 
 while True:
-    old_x, old_y = 0, 0
-    center = 0, 0
-    max_width = None
     _, frame = cam.read()
     frame = cv2.flip(frame, 1)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -27,11 +25,15 @@ while True:
 
     if landmarks:
         landmark = landmarks[0].landmark
-        points = [landmark[9]]
+        points = [landmark[8]]
         for id, mark in enumerate(points):
             x = int(mark.x*screen_w)
             y = int(mark.y*screen_h)
             cv2.circle(frame, (int(mark.x*frame_w), int(mark.y*frame_h)), 3, (0, 255, 0))
+            print(abs(x - old_x) + abs(y - old_y))
+            if abs(x - old_x) + abs(y - old_y) > 20:
+                pyautogui.moveTo(x*1.5-screen_w/4, y*1.5-screen_h/4)
+            old_x, old_y = x, y
 
             
 
